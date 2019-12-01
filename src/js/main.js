07bccebd3169d3a274a2c8d97ac4f8ab9d622a10
Matt Kokas <mattkokas@gmail.com>
2019-11-30 19:10:02 -0500

    header sticky

--- src/js/main.js
+++ src/js/main.js
@@ -28,27 +28,3 @@ navToggleClose.addEventListener('click', ev => {
    ev.preventDefault();
    navContainer.classList.remove('masthead__nav--open');
 });
-
-
-var header = document.querySelector('.masthead');
-header.classList.add('masthead--sticky');
-
-/*
-var myElement = document.querySelector('main');
-var elementWatcher = scrollMonitor.create( myElement );
-var header = document.querySelector('.masthead');
-
-var elementWatcher2 = scrollMonitor.create( header );
-
-elementWatcher2.enterViewport(function() {
-    console.log( 'I have entered the viewport' );
-   // header.classList.remove('masthead--sticky');
-});
-elementWatcher2.exitViewport(function() {
-    header.classList.add('masthead--sticky');
-});
-*/
-
-/*
-masthead__nav--open
-*/
